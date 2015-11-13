1b64b46a1661749b6af5376df845ee73ab0b7d0e
aedm <korteur@gmail.com>
2015-11-13 02:17:10 +0100

    Performs debouncing of collection updates. Fixes #19.

--- client/app.jsx
+++ client/app.jsx
@@ -60,32 +60,32 @@ App = React.createClass({
         {/* Going through your calendar */}
         <h3>{ this.text("T0015") }</h3>
         { this.text("T0016") }
-        <PlainTextQuestion question="past-flyover" />
+        <TextQuestion question="past-flyover" />
 
 
         {/* This is what my past year was about */}
         <h3>{ this.text("T0017") }</h3>
         <h4>{ this.text("T0018") }</h4>
         { this.text("T0021") }
-        <PlainTextQuestion question="past-about-family" />
+        <TextQuestion question="past-about-family" />
         { this.text("T0022") }
-        <PlainTextQuestion question="past-about-work" />
+        <TextQuestion question="past-about-work" />
         { this.text("T0023") }
-        <PlainTextQuestion question="past-about-belongings" />
+        <TextQuestion question="past-about-belongings" />
         { this.text("T0024") }
-        <PlainTextQuestion question="past-about-hobbies" />
+        <TextQuestion question="past-about-hobbies" />
         { this.text("T0025") }
-        <PlainTextQuestion question="past-about-friends" />
+        <TextQuestion question="past-about-friends" />
         { this.text("T0026") }
-        <PlainTextQuestion question="past-about-health" />
+        <TextQuestion question="past-about-health" />
         { this.text("T0027") }
-        <PlainTextQuestion question="past-about-intellectual" />
+        <TextQuestion question="past-about-intellectual" />
         { this.text("T0028") }
-        <PlainTextQuestion question="past-about-emotional" />
+        <TextQuestion question="past-about-emotional" />
         { this.text("T0029") }
-        <PlainTextQuestion question="past-about-finances" />
+        <TextQuestion question="past-about-finances" />
         { this.text("T0030") }
-        <PlainTextQuestion question="past-about-bucketlist" />
+        <TextQuestion question="past-about-bucketlist" />
         <p><i>{ this.text("T0019") }</i></p>
         <p><i>{ this.text("T0020") }</i></p>
 
@@ -93,82 +93,82 @@ App = React.createClass({
         {/* 6 sentences about my past year */}
         <h3>{ this.text("T0031") }</h3>
         { this.text("T0032") }
-        <PlainTextQuestion question="past-sixsenteces-decision" />
+        <TextQuestion question="past-sixsenteces-decision" />
         { this.text("T0033") }
-        <PlainTextQuestion question="past-sixsenteces-lesson" />
+        <TextQuestion question="past-sixsenteces-lesson" />
         { this.text("T0034") }
-        <PlainTextQuestion question="past-sixsenteces-risk" />
+        <TextQuestion question="past-sixsenteces-risk" />
         { this.text("T0035") }
-        <PlainTextQuestion question="past-sixsenteces-surprise" />
+        <TextQuestion question="past-sixsenteces-surprise" />
         { this.text("T0036") }
-        <PlainTextQuestion question="past-sixsenteces-service" />
+        <TextQuestion question="past-sixsenteces-service" />
         { this.text("T0037") }
-        <PlainTextQuestion question="past-sixsenteces-completed" />
+        <TextQuestion question="past-sixsenteces-completed" />
 
         {/* 6 questions about my past year */}
         <h3>{ this.text("T0038") }</h3>
         { this.text("T0039") }
-        <PlainTextQuestion question="past-sixquestions-proud" />
+        <TextQuestion question="past-sixquestions-proud" />
         { this.text("T0040") }
-        <PlainTextQuestion question="past-sixquestions-influencedby" />
+        <TextQuestion question="past-sixquestions-influencedby" />
         { this.text("T0041") }
-        <PlainTextQuestion question="past-sixquestions-influence" />
+        <TextQuestion question="past-sixquestions-influence" />
         { this.text("T0042") }
-        <PlainTextQuestion question="past-sixquestions-notaccomplish" />
+        <TextQuestion question="past-sixquestions-notaccomplish" />
         { this.text("T0043") }
-        <PlainTextQuestion question="past-sixquestions-discover" />
+        <TextQuestion question="past-sixquestions-discover" />
         { this.text("T0044") }
-        <PlainTextQuestion question="past-sixquestions-grateful" />
+        <TextQuestion question="past-sixquestions-grateful" />
 
         {/* Best moments */}
         <h3>{ this.text("T0045") }</h3>
         { this.text("T0046") }
-        <PlainTextQuestion question="past-bestmoments" />
+        <TextQuestion question="past-bestmoments" />
 
         {/* My three greatest accomplishments */}
         <h3>{ this.text("T0047") }</h3>
         { this.text("T0048") }
-        <PlainTextQuestion question="past-3accomplish-success" />
+        <TextQuestion question="past-3accomplish-success" />
         { this.text("T0049") }
-        <PlainTextQuestion question="past-3accomplish-achieve" />
+        <TextQuestion question="past-3accomplish-achieve" />
         { this.text("T0050") }
-        <PlainTextQuestion question="past-3accomplish-who" />
+        <TextQuestion question="past-3accomplish-who" />
 
         {/* My three biggest challenges */}
         <h3>{ this.text("T0051") }</h3>
         { this.text("T0052") }
-        <PlainTextQuestion question="past-3challenge-list" />
+        <TextQuestion question="past-3challenge-list" />
         { this.text("T0053") }
-        <PlainTextQuestion question="past-3challenge-help" />
+        <TextQuestion question="past-3challenge-help" />
         { this.text("T0054") }
-        <PlainTextQuestion question="past-3challenge-learned" />
+        <TextQuestion question="past-3challenge-learned" />
 
         {/* Forgiveness */}
         <h3>{ this.text("T0055") }</h3>
         { this.text("T0056") }
-        <PlainTextQuestion question="past-forgive" />
+        <TextQuestion question="past-forgive" />
         <p><i>{ this.text("T0057") }</i></p>
 
         {/* Letting go */}
         <h3>{ this.text("T0058") }</h3>
         { this.text("T0059") }
-        <PlainTextQuestion question="past-lettinggo" />
+        <TextQuestion question="past-lettinggo" />
 
         {/* The past year in 3 words */}
         <h3>{ this.text("T0060") }</h3>
         { this.text("T0061") }
-        <PlainTextQuestion question="past-3words" />
+        <TextQuestion question="past-3words" />
 
         {/* The book of my past year */}
         <h3>{ this.text("T0062") }</h3>
         { this.text("T0063") }
-        <PlainTextQuestion question="past-book" />
+        <TextQuestion question="past-book" />
         <p><i>{ this.text("T0064") }</i></p>
 
         {/* Farewell to your last year */}
         <h3>{ this.text("T0065") }</h3>
         { this.text("T0066") }
-        <PlainTextQuestion question="past-farewell" />
+        <TextQuestion question="past-farewell" />
 
         {/* You’re done with the past year :) */}
         <h3>{ this.text("T0067") }</h3>
@@ -185,110 +185,110 @@ App = React.createClass({
         {/* Dare to dream big */}
         <h3>{ this.text("T0073") }</h3>
         { this.text("T0074") }
-        <PlainTextQuestion question="ahead-dreambig" />
+        <TextQuestion question="ahead-dreambig" />
 
         {/* This is what my next year is about */}
         <h3>{ this.text("T0075") }</h3>
         <h4>{ this.text("T0076") }</h4>
         { this.text("T0021") }
-        <PlainTextQuestion question="ahead-about-family" />
+        <TextQuestion question="ahead-about-family" />
         { this.text("T0022") }
-        <PlainTextQuestion question="ahead-about-work" />
+        <TextQuestion question="ahead-about-work" />
         { this.text("T0023") }
-        <PlainTextQuestion question="ahead-about-belongings" />
+        <TextQuestion question="ahead-about-belongings" />
         { this.text("T0024") }
-        <PlainTextQuestion question="ahead-about-hobbies" />
+        <TextQuestion question="ahead-about-hobbies" />
         { this.text("T0025") }
-        <PlainTextQuestion question="ahead-about-friends" />
+        <TextQuestion question="ahead-about-friends" />
         { this.text("T0026") }
-        <PlainTextQuestion question="ahead-about-health" />
+        <TextQuestion question="ahead-about-health" />
         { this.text("T0027") }
-        <PlainTextQuestion question="ahead-about-intellectual" />
+        <TextQuestion question="ahead-about-intellectual" />
         { this.text("T0028") }
-        <PlainTextQuestion question="ahead-about-emotional" />
+        <TextQuestion question="ahead-about-emotional" />
         { this.text("T0029") }
-        <PlainTextQuestion question="ahead-about-finances" />
+        <TextQuestion question="ahead-about-finances" />
         { this.text("T0030") }
-        <PlainTextQuestion question="ahead-about-bucketlist" />
+        <TextQuestion question="ahead-about-bucketlist" />
         <p><i>{ this.text("T0077") }</i></p>
         <p><i>{ this.text("T0078") }</i></p>
 
         {/* The year ahead of me */}
         <h3>{ this.text("T0081") }</h3>
         { this.text("T0082") }
-        <PlainTextQuestion question="ahead-calendar" />
+        <TextQuestion question="ahead-calendar" />
 
         {/* Magical tricola for the year ahead */}
         <h3>{ this.text("T0084") }</h3>
         { this.text("T0085") }
-        <PlainTextQuestion question="ahead-triplets-lovemyself" />
+        <TextQuestion question="ahead-triplets-lovemyself" />
         { this.text("T0086") }
-        <PlainTextQuestion question="ahead-triplets-letgo" />
+        <TextQuestion question="ahead-triplets-letgo" />
         { this.text("T0087") }
-        <PlainTextQuestion question="ahead-triplets-achieve" />
+        <TextQuestion question="ahead-triplets-achieve" />
         { this.text("T0088") }
-        <PlainTextQuestion question="ahead-triplets-pillars" />
+        <TextQuestion question="ahead-triplets-pillars" />
         { this.text("T0089") }
-        <PlainTextQuestion question="ahead-triplets-discover" />
+        <TextQuestion question="ahead-triplets-discover" />
         { this.text("T0090") }
-        <PlainTextQuestion question="ahead-triplets-sayno" />
+        <TextQuestion question="ahead-triplets-sayno" />
         { this.text("T0091") }
-        <PlainTextQuestion question="ahead-triplets-cozy" />
+        <TextQuestion question="ahead-triplets-cozy" />
         { this.text("T0092") }
-        <PlainTextQuestion question="ahead-triplets-morning" />
+        <TextQuestion question="ahead-triplets-morning" />
         { this.text("T0093") }
-        <PlainTextQuestion question="ahead-triplets-pamper" />
+        <TextQuestion question="ahead-triplets-pamper" />
         { this.text("T0094") }
-        <PlainTextQuestion question="ahead-triplets-visit" />
+        <TextQuestion question="ahead-triplets-visit" />
         { this.text("T0095") }
-        <PlainTextQuestion question="ahead-triplets-connect" />
+        <TextQuestion question="ahead-triplets-connect" />
         { this.text("T0096") }
-        <PlainTextQuestion question="ahead-triplets-reward" />
+        <TextQuestion question="ahead-triplets-reward" />
 
         {/* 6 sentences about my next year */}
         <h3>{ this.text("T0097") }</h3>
         { this.text("T0098") }
-        <PlainTextQuestion question="ahead-6sentences-finally" />
+        <TextQuestion question="ahead-6sentences-finally" />
         { this.text("T0099") }
-        <PlainTextQuestion question="ahead-6sentences-bravest" />
+        <TextQuestion question="ahead-6sentences-bravest" />
         { this.text("T0100") }
-        <PlainTextQuestion question="ahead-6sentences-advise" />
+        <TextQuestion question="ahead-6sentences-advise" />
         { this.text("T0101") }
-        <PlainTextQuestion question="ahead-6sentences-habit" />
+        <TextQuestion question="ahead-6sentences-habit" />
         { this.text("T0102") }
-        <PlainTextQuestion question="ahead-6sentences-procrastinate" />
+        <TextQuestion question="ahead-6sentences-procrastinate" />
         { this.text("T0103") }
-        <PlainTextQuestion question="ahead-6sentences-energy" />
+        <TextQuestion question="ahead-6sentences-energy" />
 
         {/* 6 more sentences about my next year */}
         <h3>{ this.text("T0104") }</h3>
         { this.text("T0105") }
-        <PlainTextQuestion question="ahead-6sentences-dream" />
+        <TextQuestion question="ahead-6sentences-dream" />
         { this.text("T0106") }
-        <PlainTextQuestion question="ahead-6sentences-plan" />
+        <TextQuestion question="ahead-6sentences-plan" />
         { this.text("T0107") }
-        <PlainTextQuestion question="ahead-6sentences-feeling" />
+        <TextQuestion question="ahead-6sentences-feeling" />
         { this.text("T0108") }
-        <PlainTextQuestion question="ahead-6sentences-sayno" />
+        <TextQuestion question="ahead-6sentences-sayno" />
         { this.text("T0109") }
-        <PlainTextQuestion question="ahead-6sentences-sayyes" />
+        <TextQuestion question="ahead-6sentences-sayyes" />
         { this.text("T0110") }
-        <PlainTextQuestion question="ahead-6sentences-special" />
+        <TextQuestion question="ahead-6sentences-special" />
 
         {/* 30 day challenge */}
         <h3>{ this.text("T0111") }</h3>
         { this.text("T0129") }
-        <PlainTextQuestion question="ahead-30day" />
+        <TextQuestion question="ahead-30day" />
 
         {/* My word for the year ahead */}
         <h3>{ this.text("T0130") }</h3>
         { this.text("T0131") }
-        <PlainTextQuestion question="ahead-myword" />
+        <TextQuestion question="ahead-myword" />
 
         {/* Secret wish */}
         <h3>{ this.text("T0132") }</h3>
         { this.text("T0133") }
-        <PlainTextQuestion question="ahead-secretwish" />
+        <TextQuestion question="ahead-secretwish" />
 
         {/* I believe anything is possible this year. */}
         <h4><input type="checkbox" />{ this.text("T0134") }</h4>
--- client/question.jsx
+++ client/question.jsx
@@ -1,26 +1,70 @@
 
-PlainTextQuestion = React.createClass({
+/** Displays a plain text answer. Takes the value from the
+ * 'Answers' Mongo collection. The actual rendering is performed by
+ * a 'PlainTextQuestionCore' instance which is also responsible for
+ * debouncing the response function.
+ *
+ * Note that this mechanism doesn't support concurrent editing.
+ *
+ * Props:
+ *  'question': a token unique to the question
+ */
+TextQuestion = React.createClass({
   mixins: [ReactMeteorData],
 
-  getInitialState() {
-    return { value: "" };
-  },
-
+  /** Forwards changes from 'Answers' to 'this.data' */
   getMeteorData() {
     let document = Answers.findOne({ question: this.props.question });
     if (!document) return {};
-
-    /// Little hack to enable state updates inside getMeteorData() while avoiding infinite loops
-    if (this.state.value != document.answer) {
-      setTimeout(() => { this.setState({value: this.data.value}); }, 0);
+    return {
+      answer: document.answer
     }
-    return { value: document.answer };
+  },
+
+  /** Updates the 'Answers' collection by invoking a method on the backend. */
+  setAnswer(answer) {
+    Meteor.call("answerText", this.props.question, answer);
+  },
+
+  /** Passes the answer to the actual editor */
+  render() {
+    return (<TextQuestionCore answer={this.data.answer} onChange={this.setAnswer}/>);
+  }
+});
+
+
+/** Lets the user enter a plain text answer. Performs debouncing
+ * in order to go easy on MongoDB.
+ *
+ * Props:
+ *   'answer': the current value of the answer in Mongo.
+ *   'onChange': a callback called when the user changes the answer
+ */
+TextQuestionCore = React.createClass({
+  getInitialState() {
+    return {
+      value: this.props.answer,
+    };
+  },
+
+  componentWillMount() {
+    /** This should be performed for each instance, thus it's in 'componentWillMount' */
+    this.notifyParentDebounce = _.debounce(this.notifyParent, 3000);
+  },
+
+  /** Called when the Mongo collection was updated by any of the clients */
+  componentWillReceiveProps(nextProps) {
+    this.setState({value: nextProps.answer});
+  },
+
+  notifyParent(answer) {
+    this.props.onChange(answer);
   },
 
   onChange() {
     let answer = ReactDOM.findDOMNode(this.refs.answer).value;
     this.setState({value: answer});
-    Meteor.call("answerText", this.props.question, answer);
+    this.notifyParentDebounce(answer);
   },
 
   render() {
