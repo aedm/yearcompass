b69718f91ed462f7b249a1b64b73060c5ea66910
aedm <korteur@gmail.com>
2015-11-19 03:18:46 +0100

    Page header now looks yearcompassier.

--- webapp/client/app.jsx
+++ webapp/client/app.jsx
@@ -41,51 +41,49 @@ App = React.createClass({
     let page;
     switch (this.state.currentPage) {
       case "start":
-        page = <StartPage language={lang} onTurn={ () => this.turnPage("past") } />;
+        page = <StartPage language={lang} onTurn={ () => this.turnPage("past") }/>;
         break;
       case "past":
-        page = <PastYearPage language={lang} onTurn={ () => this.turnPage("next") } />;
+        page = <PastYearPage language={lang} onTurn={ () => this.turnPage("next") }/>;
         break;
       case "next":
-        page = <NextYearPage language={lang} onTurn={ () => this.turnPage("finish") } />;
+        page = <NextYearPage language={lang} onTurn={ () => this.turnPage("finish") }/>;
         break;
       case "finish":
-        page = <FinishPage language={lang} />;
+        page = <FinishPage language={lang}/>;
         break;
     }
     return (
-      <div className="booklet">
-        { page }
-      </div>
+        <div className="booklet">
+          { page }
+        </div>
     );
   },
 
   render() {
     return (
-      <div className="container">
-
-        <div id="header">
-          <div className="ribbon">
-            <div className="language">
-              Lang:
-              { Object.keys(translations).map((t) =>
-                <a href="#" key={t} onMouseDown={this.selectLanguage.bind(this, t)}>{t} </a> )}
+        <div>
+          <div id="header">
+            <div className="menu">
+              <LoginWrapper />
+              &nbsp;{ Object.keys(translations).map((t) =>
+                  <a href="#" key={t} onMouseDown={this.selectLanguage.bind(this, t)}>{t} </a> )}
             </div>
-            <div className="title">YearCompass</div>
-            <div className="user"><LoginWrapper /></div>
+            <div className="title">
+              <a href=""><img src="ui/logotext_small_black.png" alt="YearCompass"/></a>
+            </div>
           </div>
-        </div>
 
-        <div id="navigation">
-          { Object.keys(this.pages).map((page) =>
-            <button key={page} onClick={ () => this.turnPage(page)}
-                    className={this.state.currentPage == page ? "active" : ""}>
-              {this.pages[page]}
-            </button> )}
-        </div>
+          <div id="navigation">
+            { Object.keys(this.pages).map((page) =>
+                <button key={page} onClick={ () => this.turnPage(page)}
+                        className={this.state.currentPage == page ? "active" : ""}>
+                  {this.pages[page]}
+                </button> )}
+          </div>
 
-        { this.data.userId != null ? this.renderBooklet() : null }
-      </div>
+          { this.data.userId != null ? this.renderBooklet() : null }
+        </div>
     );
   }
 });
