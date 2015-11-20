b4884d344a6f473f3c3819b9b77b82fba169e71b
aedm <korteur@gmail.com>
2015-11-20 18:56:32 +0100

    Small tweaks of language buttons.

--- webapp/client/app.jsx
+++ webapp/client/app.jsx
@@ -61,38 +61,40 @@ App = React.createClass({
     );
   },
 
+  renderMenu() {
+    return (
+        <div className="menu pure-menu pure-menu-horizontal">
+          <ul className="pure-menu-list">
+            <li className="pure-menu-item"><LoginWrapper /></li>
+            <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+              <a href="#" id="menuLink1" className="pure-menu-link">Menu</a>
+              <ul className="pure-menu-children">
+                <li className="pure-menu-item">
+                  <a href="#" className="pure-menu-link">Logout</a>
+                </li>
+                <li className="pure-menu-item">
+                  <div id="menuLanguageSelector">
+                    { Object.keys(translations).map((t) =>
+                      <button key={t} onClick={this.selectLanguage.bind(this, t)}
+                              className="lang">{t}</button> )}
+                  </div>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+    );
+  },
+
   render() {
     return (
         <div>
           <div id="header">
-            <div className="menu">
-              <div className="pure-menu pure-menu-horizontal">
-                <ul className="pure-menu-list">
-                  <li className="pure-menu-item"><LoginWrapper /></li>
-                  <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
-                    <a href="#" id="menuLink1" className="pure-menu-link">Menu</a>
-                    <ul className="pure-menu-children">
-                      <li className="pure-menu-item">
-                        <a href="#" className="pure-menu-link">Logout</a>
-                      </li>
-                      <li className="pure-menu-item">
-                        <div>{ Object.keys(translations).map((t) =>
-                            <a href="#" key={t} onMouseDown={this.selectLanguage.bind(this, t)}>
-                              {t} </a> )}
-                        </div>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-
-            </div>
-
             <div className="title">
-              <a href="http://yearcompass.com"> <img src="ui/logotext_small_black.png"
-                                                     alt="YearCompass"/> </a>
+              <a href="http://yearcompass.com">
+                <img src="ui/logotext_small_black.png" alt="YearCompass"/> </a>
             </div>
-
+            { this.renderMenu() }
           </div>
 
           <div id="navigation">
