8ee323fa84f30f7308a2d4acefd33f82e066c29c
aedm <korteur@gmail.com>
2015-11-21 03:37:17 +0100

    Basic share buttons at the end of the finish screen. Related to #22.

--- webapp/client/pages/finish.jsx
+++ webapp/client/pages/finish.jsx
@@ -1,6 +1,27 @@
 FinishPage = React.createClass({
   mixins: [BookletPage],
 
+  renderShare() {
+    let url = "http://yearcompass.com";
+    let img = "http://www.yearcompass.com/wp-content/uploads/2014/12/zen_share1.png";
+    let title = "YearCompass";
+
+    return (
+        <p className="share">
+          <a href={`http://www.facebook.com/sharer/sharer.php?u=${url}&t=${title}`}
+             className="share-button facebook" target="_blank">Facebook</a>
+          <a href={`http://www.twitter.com/intent/tweet?url=${url}&text=${title}`}
+             className="share-button twitter" target="_blank">Twitter</a>
+          <a href={`http://pinterest.com/pin/create/button/?url=${url}&media=${img}&description=${title}`}
+             className="share-button pinterest" target="_blank">Pinterest</a>
+          <a href={`http://www.reddit.com/submit?url=${url}`}
+             className="share-button reddit" target="_blank">Reddit</a>
+          <a href={`http://plus.google.com/share?url=${url}`}
+             className="share-button google" target="_blank">Google+</a>
+        </p>
+    );
+  },
+
   render() {
     return (
         <div>
@@ -19,6 +40,8 @@ FinishPage = React.createClass({
             <footer>{ this.text("T0141") }</footer>
             <footer>{ this.text("T0142") }</footer>
 
+            <header>Share it</header>
+            { this.renderShare() }
           </div>
         </div>);
   }
